76068daa59d00ae3d5c0a32e55eaa3cc1d182e52
吴东 <511970747@qq.com>
2022-01-04 17:30:33 +0800

    chore: 修改appid和请求地址

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 // 服务器协议 production 必须是https
-export const SERVER_PROTOCOL = 'http://';
+export const SERVER_PROTOCOL = 'https://';
 // 域名
-// export const SERVER_DOMAIN = '120.79.99.231:9527';
-export const SERVER_DOMAIN = '150.158.22.228:9527';
+// export const SERVER_DOMAIN = 'wx.qiuchangtong.xyz';
+export const SERVER_DOMAIN = 'wx-test.qiuchangtong.xyz';
 // export const SERVER_DOMAIN = 'localhost:9527';
 // 后端 API 地址
 export const SERVER_API_ROOT = `${SERVER_PROTOCOL}${SERVER_DOMAIN}/api`;
