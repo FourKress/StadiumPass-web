14705f172cf298cecb1144906aca5df7ae703833
吴东 <511970747@qq.com>
2022-01-10 13:07:38 +0800

    feat: 接入支付7

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,9 @@
 // 服务器协议 production 必须是https
-export const SERVER_PROTOCOL = 'http://';
+export const SERVER_PROTOCOL = 'https://';
+// export const SERVER_PROTOCOL = 'http://';
 // 域名
 // export const SERVER_DOMAIN = 'wx.qiuchangtong.xyz';
-// export const SERVER_DOMAIN = 'wx-test.qiuchangtong.xyz';
-export const SERVER_DOMAIN = 'localhost:9527';
+export const SERVER_DOMAIN = 'wx-test.qiuchangtong.xyz';
+// export const SERVER_DOMAIN = 'localhost:9527';
 // 后端 API 地址
 export const SERVER_API_ROOT = `${SERVER_PROTOCOL}${SERVER_DOMAIN}/api`;
